da9c2aee5ef666d1a033cfdd5a3e6b84ee70c51e
Danilo Spinella <danyspin97@protonmail.com>
2019-12-07 00:05:21 +0100

    Fix musl compilation by adding TEMP_FAILURE_RETRY

--- dbus-proxy.c
+++ dbus-proxy.c
@@ -30,6 +30,15 @@
 #include <errno.h>
 
 #include "flatpak-proxy.h"
+// Taken from glibc unistd.h
+#ifndef TEMP_FAILURE_RETRY
+# define TEMP_FAILURE_RETRY(expression) \
+  (__extension__                                                              \
+    ({ long int __result;                                                     \
+       do __result = (long int) (expression);                                 \
+       while (__result == -1L && errno == EINTR);                             \
+       __result; }))
+#endif
 
 static const char *argv0;
 static GList *proxies;
